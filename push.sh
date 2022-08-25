4e0a15fbc5021c73295f838533dd1def94f9ddfd
markets2022 <flyingsir@163.com>
2022-08-25 10:40:58 +0800

    小地图雷达功能

--- push.sh
+++ push.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+ s_info=$1
+ #  判断第四个参数，不传默认8；传了数字，设置成指定的 帧数
+if [ ! -n "$1" ];then
+  s_info='init'
+else
+  echo "============" ${s_info}
+fi
  git add .
- git commit -m ‘init’
+ git commit -m ${s_info}
  git push -u origin 'main'
