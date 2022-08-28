598c19b4045f5f117d47123e954610669611d154
markets2022 <flyingsir@163.com>
2022-08-28 15:58:32 +0800

    c++

---
+++ vscode-c++/helloWorld/helloWorld.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int b = 1;
+    // auto a = b;
+    // cout << a << endl;
+    // vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    // int len = msg.size();
+    // cout << "长度============" << len << endl;
+    // for (int i=0; i<5; i++){
+    //     // msg.push_back("a");
+    //     cout << i << endl;
+    // } 
+
+    count<<"你好,c++语言\n\n";
+    count<<"我是初学者\n";
+    count<<"我like";
+    return 0;
+}
